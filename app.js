93fd1b9459ea4b4c302246e6b878fd5bae3168bb
Manojuio <undrallamanojkumar@gmail.com>
2025-12-14 21:40:10 +0530

    add middleware logger and update

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const cors = require("cors");
 const app = express();
 
 
-
+const logger = require("./src/middleware/logger.middleware.js");
 const userRoutes = require("./src/routes/user.routes.js");
 
 app.use(cors());
 
 app.use(express.json());
+app.use(logger);
 app.use("/api",userRoutes);
 
 
